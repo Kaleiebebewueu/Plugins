aadd7613946c8305970d9cd189831c416cd75e31
PAPA <sachinxxxsanatani@gmail.com>
2024-08-11 13:20:59 +0530

    Add files via upload

---
+++ STORM/modules/fuck.py
@@ -0,0 +1,35 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import SUDO_USERS
+
+@Client.on_message(
+    filters.command(["fuck"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
+async def fuck(client: Client, message: Message):
+    e = await message.edit(".                       /¯ )")
+    await e.edit(".                       /¯ )\n                      /¯  /")
+    await e.edit(
+        ".                       /¯ )\n                      /¯  /\n                    /    /"
+    )
+    await e.edit(
+        ".                       /¯ )\n                      /¯  /\n                    /    /\n              /´¯/'   '/´¯¯`•¸"
+    )
+    await e.edit(
+        ".                       /¯ )\n                      /¯  /\n                    /    /\n              /´¯/'   '/´¯¯`•¸\n          /'/   /    /       /¨¯\\ "
+    )
+    await e.edit(
+        ".                       /¯ )\n                      /¯  /\n                    /    /\n              /´¯/'   '/´¯¯`•¸\n          /'/   /    /       /¨¯\\ \n        ('(   (   (   (  ¯~/'  ')"
+    )
+    await e.edit(
+        ".                       /¯ )\n                      /¯  /\n                    /    /\n              /´¯/'   '/´¯¯`•¸\n          /'/   /    /       /¨¯\\ \n        ('(   (   (   (  ¯~/'  ')\n         \\                        /"
+    )
+    await e.edit(
+        ".                       /¯ )\n                      /¯  /\n                    /    /\n              /´¯/'   '/´¯¯`•¸\n          /'/   /    /       /¨¯\\ \n        ('(   (   (   (  ¯~/'  ')\n         \\                        /\n          \\                _.•´"
+    )
+    await e.edit(
+        ".                       /¯ )\n                      /¯  /\n                    /    /\n              /´¯/'   '/´¯¯`•¸\n          /'/   /    /       /¨¯\\ \n        ('(   (   (   (  ¯~/'  ')\n         \\                        /\n          \\                _.•´\n            \\              ("
+    )
+    await e.edit(
+        ".                       /¯ )\n                      /¯  /\n                    /    /\n              /´¯/'   '/´¯¯`•¸\n          /'/   /    /       /¨¯\\ \n        ('(   (   (   (  ¯~/'  ')\n         \\                        /\n          \\                _.•´\n            \\              (\n              \\  "
+    )
+    
